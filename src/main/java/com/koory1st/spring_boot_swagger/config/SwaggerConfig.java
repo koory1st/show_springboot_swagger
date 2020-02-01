fe8b177d6031a1ad431c3194788223ce5c53710d
koory1st <32436334@qq.com>
2020-02-01 23:01:18 +0800

    接入swagger

---
+++ src/main/java/com/koory1st/spring_boot_swagger/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.koory1st.spring_boot_swagger.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket configSwagger() {
+        return new Docket(DocumentationType.SWAGGER_2);
+    }
+}
